4d64fc908bf86bff284c47edeffb28586c4773a0
antikun <121240743+antikun@users.noreply.github.com>
2023-02-08 19:13:28 +0200

    Write basic functions

---
+++ script.js
@@ -0,0 +1,23 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, operator) {
+    if (operator === "+") return sum(a, b);
+    if (operator === "-") return subtract(a, b);
+    if (operator === "*") return multiply(a, b);
+    if (operator === "/") return divide(a, b);
+}
+
