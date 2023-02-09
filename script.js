bb42c33db23ce3ddd681542eabcf2d020de95a6a
antikun <121240743+antikun@users.noreply.github.com>
2023-02-10 00:42:10 +0200

    Write most of the logic for the calculator
    I added most of the basic functions and overall logic for the calculator.
    I need ot add event listeners to the rest of the buttons (%, ., +/-) to
    finish the calculator.

--- script.js
+++ script.js
@@ -1,58 +1,88 @@
-function sum(a, b) {
-    return a + b;
+const plusBtn = document.querySelector("#add");
+const minusBtn = document.querySelector("#subtract");
+const mulBtn = document.querySelector("#multiply");
+const divideBtn = document.querySelector("#divide");
+const resetBtn = document.querySelector("#reset");
+const equalsBtn = document.querySelector("#equals");
+
+const numButtons = [];
+const inputs = {
+    num: "0",
+    display: document.querySelector(".display-result"),
+    numsArray: [],
+    operator: []
 }
 
-function subtract(a, b) {
-    return a - b;
+for (let i = 0; i < 10; i++) {
+    numButtons.push(document.querySelector(`#btn${i}`));
+    numButtons[i].addEventListener("click", (e) => {
+        e.preventDefault();
+        if (inputs.num === "0") {
+            inputs.num = `${i}`;
+        } else {
+            inputs.num += `${i}`;
+        }
+        inputs.display.textContent = inputs.num;
+    })
 }
 
-function multiply(a, b) {
-    return a * b;
-}
+function sum(a, b) { return a + b; }
 
-function divide(a, b) {
-    return a / b;
-}
+function subtract(a, b) { return a - b; }
 
-function operate(a, b, operator) {
-    if (b == "") return;
+function multiply(a, b) { return a * b; }
+
+function divide(a, b) { return a / b; }
+
+function makeCalculation(a, b, operator) {
+    if (b == undefined || operator === undefined) return;
     if (operator === "+") return sum(a, b);
     if (operator === "-") return subtract(a, b);
     if (operator === "*") return multiply(a, b);
     if (operator === "/") return divide(a, b);
 }
 
-const numButtons = [];
-for (let i = 0; i < 10; i++) {
-    numButtons.push(document.querySelector(`#btn${i}`));
-}
-
-const plusBtn = document.querySelector("#add");
-
-const inputs = {
-    display: document.querySelector(".display-result"),
-    numsArray: [],
-    operator: ""
-}
-
-// numButtons.forEach(button => addEventListener("click", function (e) {
-//     e.preventDefault();
-// }))
-
-plusBtn.addEventListener("click", function () {
-    inputs.numsArray.push(parseInt(inputs.display.textContent));
-    inputs.operator = "+";
-    const result = operate(inputs.numsArray[0], inputs.numsArray[1], inputs.operator);
+function displayResult() {
+    const result = makeCalculation(inputs.numsArray[0], inputs.numsArray[1], inputs.operator[0]);
     if (result == undefined) {
         return;
     } else {
-        inputs.numsArray.push(result);
+        if (inputs.numsArray.length < 2) {
+            inputs.numsArray.push(result);
+        }
+        if (inputs.numsArray.length >= 2) {
+            inputs.numsArray.splice(0, 2);
+            inputs.numsArray.push(result);
+        }
         inputs.display.textContent = result;
-    }
-})
+    };
+}
 
-numButtons[1].addEventListener("click", () => {
-    let num = "";
-    inputs.display.textContent = (num += "1");
-    // inputs.num1 = parseInt(inputs.display.textContent);
+function operate(symbol) {
+    inputs.numsArray.push(parseInt(inputs.num));
+    inputs.num = "";
+    inputs.operator.push(symbol.toString())
+    displayResult();
+    inputs.operator.unshift(symbol.toString());
+}
+
+function reset() {
+    inputs.numsArray.splice(0, 2);
+    inputs.display.textContent = "0";
+    inputs.num = "0";
+    inputs.operator.splice(0, inputs.operator.length);
+}
+
+plusBtn.addEventListener("click", () => operate("+"));
+minusBtn.addEventListener("click", () => operate("-"));
+mulBtn.addEventListener("click", () => operate("*"));
+divideBtn.addEventListener("click", () => operate("/"))
+
+resetBtn.addEventListener("click", reset);
+equalsBtn.addEventListener("click", () => {
+    inputs.numsArray.push(parseInt(inputs.num));
+    displayResult();
+    inputs.num = inputs.display.textContent;
+    inputs.operator.splice(0, inputs.operator.length);
+    inputs.numsArray.pop();
 })
